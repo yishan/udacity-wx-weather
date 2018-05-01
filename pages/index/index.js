7186588471f68ad71b767c283325ae3298a29954
liyishan <hycosti@gmail.com>
2018-05-01 15:51:08 +0800

    0.6.9

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,12 +16,27 @@ const weatherColorMap = {
   'snow': '#aae1fc'
 }
 
+const UNPROMPTED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+
+const UNPROMPTED_TIPS = "点击获取当前位置"
+const UNAUTHORIZED_TIPS = "点击开启位置权限"
+const AUTHORIZED_TIPS = ""
+
+const QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+
 Page({
   data: {
-    nowTemp: '14°',
-    nowWeather: '多云',
+    nowTemp: '',
+    nowWeather: '',
     nowWeatherBackground: '',
-    hourlyWeather: []
+    hourlyWeather: [],
+    todayTemp:'',
+    todayDate:'',
+    city:'广州',
+    locationAuthType: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
   onPullDownRefresh() {
     this.getNow(()=>{
@@ -29,18 +44,36 @@ Page({
     })
   },
   onLoad(){
+    this.qqmapsdk = new QQMapWX({
+      key: '5KIBZ-5VNK6-U6ASB-EL4A3-Q433K-L5FTW'
+    })
     this.getNow()
+  },
+  onShow(){
+    wx.getSetting({
+      success: res=>{
+      let auth = res.authSetting['scope.userLocation']
+      if (auth && this.data.locationAuthType != AUTHORIZED){
+        this.setData({
+          locationAuthType: AUTHORIZED,
+          locationTipsText: AUTHORIZED_TIPS
+        })
+        this.getLocation()
+      }
+    }
+  })
   },
   getNow(callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
       data: {
-        city: '北京市',
+        city: this.data.city,
       },
       success: res => {
         let result = res.data.result
         this.setNow(result)
         this.setHourlyWeather(result)
+        this.setToday(result)
       },
       complete: ()=>{
         callback && callback()
@@ -77,6 +110,54 @@ setHourlyWeather(result){
   hourlyWeather[0].time = '现在'
   this.setData({
     hourlyWeather: hourlyWeather
+  })
+},
+setToday(result){
+  let date = new Date()
+  this.setData({
+    todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+    todayDate: `${date.getFullYear()} - ${date.getMonth()+1} - ${date.getDate()} 今天`
+  })
+},
+onTapDayWeather(){
+  wx.navigateTo({
+    url: '/pages/list/list?city=' + this.data.city
+  })
+},
+onTapLocation(){
+  if (this.data.locationAuthType === UNAUTHORIZED)
+    wx.openSetting()
+  else
+    this.getLocation()
+},
+getLocation(){
+  wx.getLocation({
+    success: res=>{
+      this.setData({
+        locationAuthType: AUTHORIZED,
+        locationTipsText: AUTHORIZED_TIPS
+      })
+      this.qqmapsdk.reverseGeocoder({
+        location: {
+          latitude: res.latitude,
+          longitude: res.longitude
+        },
+        success: res => {
+          let city = res.result.address_component.city
+          this.setData({
+            city: city,
+            locationTipsText: ''
+          })
+          this.getNow()
+        }
+      })
+    },
+    fail: () => {
+      this.setData({
+        locationAuthType: UNAUTHORIZED,
+        locationTipsText: UNAUTHORIZED_TIPS
+      })
+    }
   })
 }
 })
